dd61c1c0743e91a29d4b38cc44cd2e1dba9e62f0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-25 08:10:29 -0400

    feat: Add type basics and type inference examples

---
+++ src/type-basics.js
@@ -0,0 +1,18 @@
+"use strict";
+// TYPE BASICS
+let age = 30;
+let firstName = 'Mario';
+let isFictional;
+age = 31;
+firstName = 'Michael';
+isFictional = true;
+// TYPE INFERENCE
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+planet = 'Saturn';
+moons = 145;
+isLarge = true;
+// NULL & UNDEFINED
+let something;
+let anotherThing;
