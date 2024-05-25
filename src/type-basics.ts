8d778f0684e52b7ce352438b4051c5670f4ccd8d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-25 08:27:14 -0400

    chore: Update code with recent changes

---
+++ src/type-basics.ts
@@ -0,0 +1,21 @@
+// TYPE BASICS
+let age: number = 30
+let firstName: string = 'Mario'
+let isFictional: boolean
+
+age = 31
+firstName = 'Michael'
+isFictional = true
+
+// TYPE INFERENCE
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+planet = 'Saturn'
+moons = 145
+isLarge = true
+
+// NULL & UNDEFINED
+let something = undefined
+let anotherThing = null
