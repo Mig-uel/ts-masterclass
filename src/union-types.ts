9bd049e7ca9f3acd638968c3d10ce82947c8bb3a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-02 14:31:03 -0400

    feat: Add union-types.ts file with examples and explanations

---
+++ src/union-types.ts
@@ -0,0 +1,30 @@
+// ------ UNION TYPES ----- //
+
+let someId: number | string // cam be either type number or string
+someId = 1
+someId = '2'
+
+let email: string | null = null
+email = 'mario@email.com'
+email = null
+
+type Id = number | string // type alias of type number or string
+let anotherId: Id
+anotherId = '1kj1b2eqwjbf'
+anotherId = 3298230901
+
+// ---- UNION TYPE CONS --- //
+
+/**
+ * @desc A function that takes in an id and swaps it
+ * @param id
+ * @returns {Id}
+ */
+function swapIdType(id: Id): Id {
+  // can only use props and methods common to
+  // both number and string types
+  // parseInt(id) --> not allowed
+
+  return id
+}
+swapIdType(5)
