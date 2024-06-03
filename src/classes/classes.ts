3ab6c49a3e739e92688ac244f1712960a4b9f4f2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-03 18:11:39 -0400

    feat: Add methods to add and remove toppings in Pizza class

--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -13,6 +13,17 @@ class Pizza {
   price: number
   base: Base = 'classic'
   toppings: string[] = []
+
+  addToppings(topping: string): void {
+    this.toppings.push(topping)
+  }
+  removeTopping(topping: string): void {
+    this.toppings = this.toppings.filter((t) => t !== topping)
+  }
 }
 
 const pizza = new Pizza('mario special', 15)
+
+pizza.addToppings('mushrooms')
+pizza.addToppings('four cheese blend')
+pizza.removeTopping('mushrooms')
