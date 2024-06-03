d9b82af0f3b9a27dbd23208e04442acbcb516f48
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-03 18:38:53 -0400

    refactor: Add getter for menu item details in MenuItem class

--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -6,6 +6,10 @@ type Base = 'classic' | 'thick' | 'thin' | 'garlic'
 // ------ INHERITANCE ----- //
 class MenuItem {
   constructor(private title: string, private price: number) {}
+
+  get details(): string {
+    return `${this.title} - $${this.price}`
+  }
 }
 
 class Pizza extends MenuItem {
@@ -36,7 +40,10 @@ pizza.removeTopping('mushrooms')
 //pizza.selectBase('crispy') // error - param does not adhere to the type Base
 pizza.selectBase('garlic')
 
-console.log(pizza)
+function printMenuItem(item: MenuItem): void {
+  console.log(item.details)
+}
+printMenuItem(pizza)
 
 // --- CLASSES AS TYPES --- //
 const pizzaOne: Pizza = new Pizza('mario special', 15)
