902b9e3a792d44260c41f139c85bdcdb91c27702
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 19:53:45 -0400

    feat: Add form submission functionality to create page

--- src/web/src/ts/create.ts
+++ src/web/src/ts/create.ts
@@ -1,13 +1,24 @@
-const titleInput = document.querySelector(
-  'input[name="title"]'
-) as HTMLInputElement // type assertion
-
-const descriptionInput = document.querySelector(
-  'textarea'
-) as HTMLTextAreaElement // type assertion
+import { Pizza, PizzaProps } from './models/pizza.model'
 
 const form = document.querySelector('.create') as HTMLFormElement // type assertion
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
+
+  const data = new FormData(form)
+
+  const newPizza: PizzaProps = {
+    title: data.get('title') as string,
+    description: data.get('description') as string,
+    toppings: data.getAll('toppings') as string[],
+    price: parseInt(data.get('price') as string),
+  }
+
+  const res = await Pizza.save(newPizza)
+
+  if (!res.ok) {
+    alert('Not able to save the pizza')
+  }
+
+  if (res.ok) window.location.href = '/'
 })
