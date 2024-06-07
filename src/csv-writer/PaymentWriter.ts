9316ebc9d6c10458d37cddcaf018a63f40b41562
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 17:50:04 -0400

    refactor: Update PaymentWriter to use generic Payment type

--- src/csv-writer/PaymentWriter.ts
+++ src/csv-writer/PaymentWriter.ts
@@ -1,6 +1,7 @@
 import { CSVWriter } from './app'
+import { Payment } from './interfaces/payment'
 
-const paymentWriter = new CSVWriter(['id', 'amount', 'to', 'notes'])
+const paymentWriter = new CSVWriter<Payment>(['id', 'amount', 'to', 'notes'])
 
 paymentWriter.addRows([
   { id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
