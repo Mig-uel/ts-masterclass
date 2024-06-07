d6e777dd2f81f2e8a1e55316cc19830fab68618f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 17:42:41 -0400

    refactor: Update CSVWriter to use generics for improved flexibility

--- src/csv-writer/app.ts
+++ src/csv-writer/app.ts
@@ -2,10 +2,8 @@
 import { appendFileSync } from 'fs'
 import { Payment } from './interfaces/payment'
 
-type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[]
-
-class CSVWriter {
-  constructor(private columns: PaymentColumns) {
+export class CSVWriter<T> {
+  constructor(private columns: (keyof T)[]) {
     this.csv = this.columns.join(',') + '\n'
   }
 
@@ -18,7 +16,7 @@ class CSVWriter {
     console.log('FILE SAVED TO ', filename)
   }
 
-  addRows(val: Payment[]): void {
+  addRows(val: T[]): void {
     let rows = val.map((v) => this.formatRow(v))
 
     this.csv += rows.join('\n')
@@ -26,15 +24,7 @@ class CSVWriter {
     console.log(this.csv)
   }
 
-  private formatRow(obj: Payment): string {
+  private formatRow(obj: T): string {
     return this.columns.map((col) => obj[col]).join(',')
   }
 }
-
-const writer = new CSVWriter(['id', 'amount', 'to', 'notes'])
-writer.addRows([
-  { id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
-  { id: 2, amount: 100, to: 'luigi', notes: 'for ghostbusting' },
-])
-
-writer.save('payments')
