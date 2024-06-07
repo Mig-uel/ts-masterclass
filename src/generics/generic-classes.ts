dc20d9e02d1464652f06759e3d99770d88bec089
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 16:57:00 -0400

    feat: Add user data to DataCollection and implement loadOne and loadAll methods

--- src/generics/generic-classes.ts
+++ src/generics/generic-classes.ts
@@ -17,3 +17,22 @@ class DataCollection<T> {
     return this.data
   }
 }
+
+interface User {
+  name: string
+  score: number
+}
+
+const users = new DataCollection<User>([
+  { name: 'shaun', score: 125 },
+  { name: 'mario', score: 100 },
+  { name: 'peach', score: 150 },
+])
+
+users.add({ name: 'luigi', score: 100 })
+
+const user = users.loadOne()
+const userArr = users.loadAll()
+
+console.log('load one - ', user)
+console.log('load one - ', userArr)
