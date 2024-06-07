f3b3505a584011bf622dad104f5a20abb51e1acd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 18:57:05 -0400

    feat: Update DataResource class to handle CRUD operations

--- src/web/src/ts/services/DataResource.ts
+++ src/web/src/ts/services/DataResource.ts
@@ -1,8 +1,30 @@
 export class DataResource<T> {
   constructor(private endpoint: string) {}
 
-  async getAll() {}
-  async getOne(id: number) {}
-  async deleteOne(id: number) {}
-  async save(data: T) {}
+  async getAll(): Promise<T[]> {
+    const res = await fetch(this.endpoint)
+    return res.json()
+  }
+
+  async getOne(id: number): Promise<T> {
+    const res = await fetch(`${this.endpoint}/${id}`)
+    return res.json()
+  }
+
+  async deleteOne(id: number): Promise<Response> {
+    const res = await fetch(`${this.endpoint}/${id}`, {
+      method: 'DELETE',
+    })
+    return res
+  }
+
+  async save(data: T): Promise<Response> {
+    const res = await fetch(`${this.endpoint}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    return res
+  }
 }
