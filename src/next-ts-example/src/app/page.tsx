75e244dcfb000ebf250b312a3ede618a4d87c7f2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 20:38:20 -0400

    feat: Fetch and display posts on Home page

--- src/next-ts-example/src/app/page.tsx
+++ src/next-ts-example/src/app/page.tsx
@@ -1,10 +1,29 @@
 import PostCard from '@/components/post-card.component'
 
-export default function Home() {
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+
+  if (!res.ok) alert('Could not fetch the posts')
+
+  return res.json()
+}
+
+export default async function Home() {
+  const posts: Post[] = await fetchPosts()
+
   return (
     <main>
       <h2>Home</h2>
-      <PostCard title='some title' author='mario' />
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </main>
   )
 }
