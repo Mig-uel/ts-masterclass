9a420502d099a233d460367faef05c65b1fa268f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 20:16:10 -0400

    intit next.js app

---
+++ src/next-ts-example/src/app/about/page.tsx
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        laboriosam ratione dolore ullam dolorem aliquam dolorum deleniti
+        accusamus amet perspiciatis nulla animi aperiam dolor, ipsa
+        necessitatibus qui impedit voluptatibus dicta.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+        voluptatum nesciunt voluptas! Quibusdam eligendi excepturi tempore!
+        Eveniet vel ea praesentium pariatur nihil, sit adipisci a ipsum
+        consequuntur neque iste, beatae at dignissimos fugiat laboriosam
+        recusandae molestiae voluptates id sed aliquid saepe. Consequuntur
+        officia esse perspiciatis itaque voluptas debitis quia accusamus saepe
+        ad. Eos, accusamus corporis. Molestiae veniam ullam obcaecati fuga quis!
+        Fuga debitis vitae vero atque consectetur officiis? Modi, voluptatem
+        quo. Velit, iure sint. Quibusdam est ipsum tempora aperiam quo sequi
+        amet nostrum inventore omnis veritatis odio tenetur quis laudantium
+        voluptatibus ducimus itaque, numquam facere, fugiat quas sit voluptatum
+        accusantium.
+      </p>
+    </main>
+  )
+}
+
+export default About
