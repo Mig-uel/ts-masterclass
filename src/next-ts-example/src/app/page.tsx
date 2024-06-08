3dea80966bc6542d9a9d673639796a31f0f92f7e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-07 20:26:32 -0400

    feat: Add PostCard component to Home page

--- src/next-ts-example/src/app/page.tsx
+++ src/next-ts-example/src/app/page.tsx
@@ -1,7 +1,10 @@
+import PostCard from '@/components/post-card.component'
+
 export default function Home() {
   return (
     <main>
       <h2>Home</h2>
+      <PostCard title='some title' author='mario' />
     </main>
   )
 }
