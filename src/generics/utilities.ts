411b85436a0c4d3f841817c241579e429d2f897a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-01 11:37:28 -0500

    feat: Add createCourseGoal function for course management
    - Introduces a utility function to create a CourseGoal object
    - Utilizes TypeScript's Partial type for flexible goal creation

---
+++ src/generics/utilities.ts
@@ -0,0 +1,17 @@
+/** Partial Utility */
+
+interface CourseGoal {
+  title: string
+  desc: string
+  completeUntil: Date
+}
+
+function createCourseGoal(title: string, desc: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}
+
+  courseGoal.title = title
+  courseGoal.desc = desc
+  courseGoal.completeUntil = date
+
+  return courseGoal as CourseGoal
+}
