06bd90b8c4820c5feaadbba607e901ccd4a1eae3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-01 10:53:36 -0500

    refactor: Simplify merge function usage example in app.ts
    - Updated the example to allow TypeScript to infer types
    - Removed explicit type parameters for cleaner code

--- src/generics/app.ts
+++ src/generics/app.ts
@@ -5,4 +5,6 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U): T & U {
 type ObjectA = { name: string }
 type ObjectB = { age: 30 }
 
-const mergedObject = merge<ObjectA, ObjectB>({ name: 'Max' }, { age: 30 })
+// TypeScript can assume the types of the values passed or you can define them
+// const mergedObject = merge<ObjectA, ObjectB>({ name: 'Max' }, { age: 30 })
+const mergedObject = merge({ name: 'Max' }, { age: 30 })
